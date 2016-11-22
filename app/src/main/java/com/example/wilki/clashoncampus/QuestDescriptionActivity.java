e6293f8298104f61ff26c1b10d154633eeddd59d
kizzlescruff <jw478@sussex.ac.uk>
2016-11-22 15:55:41 +0000

    Fixing return to quest list bug

--- app/src/main/java/com/example/wilki/clashoncampus/QuestDescriptionActivity.java
+++ app/src/main/java/com/example/wilki/clashoncampus/QuestDescriptionActivity.java
@@ -4,20 +4,37 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 public class QuestDescriptionActivity extends AppCompatActivity {
+
+    private QuestHolder qHolder;
+    private Button playQstBtn;
+    private Button rtnToQLBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quest_description);
+        playQstBtn = (Button) findViewById(R.id.playQuestBtn);
+        rtnToQLBtn = (Button) findViewById(R.id.backToQuestListBtn);
+        qHolder = (QuestHolder) getIntent().getSerializableExtra("QUESTS");
+    }
+
+    public void onRestart(){
+        playQstBtn.setBackgroundResource(R.drawable.button);
+        rtnToQLBtn.setBackgroundResource(R.drawable.button);
     }
 
     public void playQuest(View view){
+        playQstBtn.setBackgroundResource(R.drawable.clicked_button);
         startActivity(new Intent(this, QuestMapActivity.class));
     }
 
     public void returnToQuestList(View view){
-        startActivity(new Intent(this, QuestListActivity.class));
+        rtnToQLBtn.setBackgroundResource(R.drawable.clicked_button);
+        Intent intent = new Intent(getApplicationContext(), QuestListActivity.class);
+        intent.putExtra("QUESTS", qHolder);
+        startActivity(intent);
     }
 }
